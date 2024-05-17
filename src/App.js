9dd6d701aade4ddc12c031097708020836ef9b82
farhan khan <84739370+farhankhan111@users.noreply.github.com>
2024-05-17 19:28:55 +0500

    new

--- src/App.js
+++ src/App.js
@@ -5,26 +5,36 @@ import Orders from './components/admin/orders/Orders';
 import Users from './components/admin/users/Users';
 import AddUser from './components/admin/users/Add';
 import OrderDetail from './components/admin/orders/Detail';
+import ProtectedRoute from "./utils/ProtectedRoute";
+import PrivateRoute from './utils/PrivateRoute';
 
 import axios from "axios";
 
 axios.defaults.baseURL = 'http://example-app.test/api';
-
+const isAuthenticated = false;
 const App = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/admin/dashboard" element={<Dashboard />} />
-        <Route path="/admin/orders" element={<Orders />} />
-        <Route path="/admin/order/:orderId" element={<OrderDetail />} />
+    return (
+        <Router>
+            <Routes>
 
-        <Route path="/admin/users" element={<Users />} />
-        <Route path="/admin/users/create" element={<AddUser />} />
+                <Route path="/" element={<Home />} />
+                <Route
+                    path="/admin/dashboard"
+                    element={
+                        <PrivateRoute isAuthenticated={isAuthenticated}>
+                            <Dashboard />
+                        </PrivateRoute>
+                    }
+                />
+                <Route path="/admin/orders" element={<Orders />} />
+                <Route path="/admin/order/:orderId" element={<OrderDetail />} />
 
-      </Routes>
-    </Router>
-  );
+                <Route path="/admin/users" element={<Users />} />
+                <Route path="/admin/users/create" element={<AddUser />} />
+
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/utils/ProtectedRoute.js
@@ -0,0 +1,15 @@
+import {Navigate, useLocation} from "react-router-dom";
+
+const ProtectedRoute = ({children}) => {
+    //const user = useSelector((state) => state.user);
+    //let location = useLocation();
+
+    //if(!user.state.isAuthenticated) {
+    //if(1===1) {
+    //    return <Navigate to="/login" state={{ from: location}} replace />
+    //}
+    //return children
+
+};
+
+export default ProtectedRoute;
